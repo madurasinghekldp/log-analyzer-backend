9537bf9229ee44bbe2823d22cdba9a8782e5e3ef
madurasinghekldp <madurasinghekldp@gmail.com>
2025-08-11 17:32:57 +0530

    added ErrorLog class with fields for id, timestamp, level, and message

--- src/main/java/org/example/dto/ErrorLog.java
+++ src/main/java/org/example/dto/ErrorLog.java
@@ -1,4 +1,46 @@
 package org.example.dto;
 
+
+
+import java.util.UUID;
+
 public class ErrorLog {
+    private UUID id;
+    private String timestamp;
+    private String level;
+    private String message;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
 }
