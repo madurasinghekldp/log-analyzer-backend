78349e15fe8c799dd233b5e08948f862a8176bed
madurasinghekldp <madurasinghekldp@gmail.com>
2025-08-11 22:59:30 +0530

    added @Repository and @Service annotations to ErrorLogRepository and ErrorLogService, respectively; implemented ErrorLogServiceImpl with repository dependency injection

--- src/main/java/org/example/repository/ErrorLogRepository.java
+++ src/main/java/org/example/repository/ErrorLogRepository.java
@@ -2,8 +2,10 @@ package org.example.repository;
 
 import org.example.entity.ErrorLogEntity;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ErrorLogRepository extends CassandraRepository<ErrorLogEntity, UUID> {
 }
--- src/main/java/org/example/service/impl/ErrorLogServiceImpl.java
+++ src/main/java/org/example/service/impl/ErrorLogServiceImpl.java
@@ -1,6 +1,13 @@
 package org.example.service.impl;
 
+import org.example.repository.ErrorLogRepository;
 import org.example.service.ErrorLogService;
 
 public class ErrorLogServiceImpl implements ErrorLogService {
+
+    private final ErrorLogRepository repository;
+
+    public ErrorLogServiceImpl(ErrorLogRepository repository) {
+        this.repository = repository;
+    }
 }
