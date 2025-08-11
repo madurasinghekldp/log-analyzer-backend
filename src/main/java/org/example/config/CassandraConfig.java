9d7ddd9b70493bb22f743839e8bba24174b81df9
madurasinghekldp <madurasinghekldp@gmail.com>
2025-08-11 11:22:34 +0530

    created cassandra config class

---
+++ src/main/java/org/example/config/CassandraConfig.java
@@ -0,0 +1,20 @@
+package org.example.config;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetSocketAddress;
+
+@Configuration
+public class CassandraConfig {
+
+    @Bean
+    public CqlSession cassandraSession() {
+        return CqlSession.builder()
+                .addContactPoint(new InetSocketAddress("127.0.0.1", 9042))
+                .withLocalDatacenter("datacenter1")
+                .withKeyspace("loganalytics")
+                .build();
+    }
+}
