1c1bdbc951b742fd5b54c4a5ea8976976d5fb665
madurasinghekldp <madurasinghekldp@gmail.com>
2025-08-14 10:01:45 +0530

    added @CrossOrigin annotation to LogStreamController to enable CORS for log streaming endpoint

--- src/main/java/org/example/controller/LogStreamController.java
+++ src/main/java/org/example/controller/LogStreamController.java
@@ -1,6 +1,7 @@
 package org.example.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import java.time.Duration;
 import reactor.core.publisher.Flux;
 
 @RestController
+@CrossOrigin
 public class LogStreamController {
 
     @GetMapping(value = "/logs/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
